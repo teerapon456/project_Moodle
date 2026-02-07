9f8bf8012454acbe24982444c8125898e5be4a6d
MyHR Admin <admin@myhr.local>
2026-02-07 16:26:15 +0700

    fix: Update CarBooking and Dormitory login modules

--- Modules/Dormitory/login.php
+++ Modules/Dormitory/login.php
@@ -127,13 +127,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
                     ชื่อผู้ใช้ / อีเมล
                 </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" name="username" type="text" placeholder="Username or Email" required>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" name="username" type="text" placeholder="ระบุชื่อผู้ใช้ หรือ อีเมล" required>
             </div>
             <div class="mb-6">
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
                     รหัสผ่าน
                 </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" name="password" type="password" placeholder="******************" required>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" name="password" type="password" placeholder="ระบุรหัสผ่าน" required>
             </div>
             <div class="flex items-center justify-between">
                 <button class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full" type="submit">
